55c5b475669e059915188a50075c82954655c53f
sojess <work.sojess@gmail.com>
2022-05-21 18:00:31 +0100

    routes added for sorting by category

--- src/app/components/product-list/product-list.component.ts
+++ src/app/components/product-list/product-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 import { Product } from 'src/app/common/product';
 import { ProductService } from 'src/app/services/product.service';
 
@@ -10,14 +11,36 @@ import { ProductService } from 'src/app/services/product.service';
 export class ProductListComponent implements OnInit {
 
   products: Product[];
+  currentCategoryId: number;
 
-  constructor(private productService: ProductService) { }
+  constructor(
+    private productService: ProductService,
+    private route: ActivatedRoute
+    ) {console.log("instance created for category id:"+ route.url); }
 
   ngOnInit(): void {
-    this.listProducts();
+    this.route.paramMap.subscribe(() => {
+      this.listProducts();
+    });
+    
   }
   listProducts(){
-    this.productService.getProductList().subscribe(
+    //check if id parameter is available
+    const hasCategoryId: boolean = this.route.snapshot.paramMap.has('id');
+    console.log("has categoryId:"+ hasCategoryId);
+
+    //if id is available convert it into a number
+
+    if(hasCategoryId){
+      this.currentCategoryId = Number(this.route.snapshot.paramMap.get('id'));
+      console.log(this.currentCategoryId);
+    }else{
+
+      //set default value as 1
+      this.currentCategoryId=0;
+      console.log(this.currentCategoryId);
+    }
+    this.productService.getProductList(this.currentCategoryId).subscribe(
       data => {
         this.products=data;
       }
--- src/app/services/product.service.ts
+++ src/app/services/product.service.ts
@@ -13,11 +13,16 @@ export class ProductService {
 
   constructor(private httpClient: HttpClient) { }
 
-  getProductList(): Observable<Product[]> {
-
-    return this.httpClient.get<GetResponse>(this.baseUrl).pipe(
-      map(response => response._embedded.products)
-    );
+  getProductList(theCategoryId: number): Observable<Product[]> {
+    
+    console.log("product service is called for category id:" +theCategoryId);
+    
+    const searchUrl= theCategoryId==0?this.baseUrl:`${this.baseUrl}/search/findByCategoryId?id=${theCategoryId}`;
+    
+      return this.httpClient.get<GetResponse>(searchUrl).pipe(
+        map(response => response._embedded.products)
+      );
+    
   }
 }
 
