1b87546ee277389a46a2f220c48644a047d9c658
sojess <work.sojess@gmail.com>
2022-05-21 19:06:29 +0100

    added dynamic loading of categories

--- src/app/services/product.service.ts
+++ src/app/services/product.service.ts
@@ -3,6 +3,7 @@ import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { Product } from '../common/product';
 import { map } from 'rxjs/operators';
+import { ProductCategory } from '../common/product-category';
 
 @Injectable({
   providedIn: 'root'
@@ -10,6 +11,7 @@ import { map } from 'rxjs/operators';
 export class ProductService {
 
   private baseUrl="http://localhost:8080/api/products";
+  private categoryUrl="http://localhost:8080/api/product-category"
 
   constructor(private httpClient: HttpClient) { }
 
@@ -19,15 +21,26 @@ export class ProductService {
     
     const searchUrl= theCategoryId==0?this.baseUrl:`${this.baseUrl}/search/findByCategoryId?id=${theCategoryId}`;
     
-      return this.httpClient.get<GetResponse>(searchUrl).pipe(
+      return this.httpClient.get<GetResponseProducts>(searchUrl).pipe(
         map(response => response._embedded.products)
       );
     
   }
+  getProductCategories(): Observable<ProductCategory[]> {
+
+    return this.httpClient.get<GetResponseProductCategories>(this.categoryUrl).pipe(
+      map(response => response._embedded.productCategory)
+    );
+  }
 }
 
-interface GetResponse {
+interface GetResponseProducts {
   _embedded:{
     products:Product[];
+  }
+}
+interface GetResponseProductCategories {
+  _embedded:{
+    productCategory:ProductCategory[];
   }
 }
